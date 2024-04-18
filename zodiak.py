4b963b88e0f490501bbe279048dfce4b9afe4270
dipost2007 <167181276+dipost2007@users.noreply.github.com>
2024-04-18 20:49:46 +0800

    Create zodiak.py
    new file

---
+++ zodiak.py
@@ -0,0 +1,28 @@
+years = [
+    "Дракон",
+    "Змея",
+    "Лошадь",
+    "Овца",
+    "Обезьяна",
+    "Петух",
+    "Собака",
+    "Свинья",
+    "Крыса",
+    "Бык",
+    "Тигр",
+    "Заяц",
+]
+year = int(input())
+
+
+def is_years(year):
+    if year % 12 == 0:
+        res = 12 - 8
+    else:
+        res = (year % 12) - 8
+    # if res < 0:
+    # res -= 1
+    return years[res]
+
+
+print(is_years(year))
