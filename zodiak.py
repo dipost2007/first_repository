e6398569ec1df9c2a5ff784f8889b410ca098474
dipost2007 <167181276+dipost2007@users.noreply.github.com>
2024-04-18 21:37:26 +0800

    test OK

--- zodiak.py
+++ zodiak.py
@@ -22,7 +22,7 @@ def is_years(year):
         res = (year % 12) - 8
     # if res < 0:
     # res -= 1
-    return years[res] # проверка
+    return years[res] # проверка синхры
 
 
 print(is_years(year))
